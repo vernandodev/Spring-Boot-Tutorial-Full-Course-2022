9ad4754718e833dd3bac74ee988066e2c3f618ed
Richo Vernando <66782281+vernandodev@users.noreply.github.com>
2023-04-06 10:15:22 +0700

    inside of springboot application execution process

---
+++ springboot-internals/springboot-internals/src/main/java/com/springboot/springbootinternals/SpringbootInternalsApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootinternals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInternalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootInternalsApplication.class, args);
+	}
+
+}
