280ecde8bec57b58662db3b15dc5872e78be04cb
Richo Vernando <66782281+vernandodev@users.noreply.github.com>
2023-04-06 10:16:31 +0700

    add springboot-first-app

--- springboot-demo/springboot-demo/src/main/java/com/springboot/springbootdemo/SpringbootDemoApplication.java
+++ springboot-demo/springboot-demo/src/main/java/com/springboot/springbootdemo/SpringbootDemoApplication.java
@@ -1,14 +1,17 @@
 package com.springboot.springbootdemo;
 
+import com.springboot.springbootdemo.sub1.SpringComponent1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringbootDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootDemoApplication.class, args);
-		System.out.println("Spring Boot Hello World !");
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringbootDemoApplication.class, args);
+		SpringComponent1 springComponent1 = applicationContext.getBean(SpringComponent1.class);
+		System.out.println(springComponent1.getMessage());
 	}
 
 }
