1d70b1d4c58f0def5d8a33b74b656f06d96e8f8d
Richo Vernando <66782281+vernandodev@users.noreply.github.com>
2023-04-28 09:56:08 +0700

    update

--- student-system/springboot-student-system/springboot-student-system/src/main/java/com/springboot/springbootstudentsystem/controller/StudentController.java
+++ student-system/springboot-student-system/springboot-student-system/src/main/java/com/springboot/springbootstudentsystem/controller/StudentController.java
@@ -2,15 +2,18 @@ package com.springboot.springbootstudentsystem.controller;
 
 import com.springboot.springbootstudentsystem.entity.Student;
 import com.springboot.springbootstudentsystem.service.StudentService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/student")
+@RequestMapping("/api")
 public class StudentController {
+    public static final String STUDENT = "/student";
     private StudentService studentService;
+    @GetMapping(value = STUDENT)
     public List<Student> getAllStudent() {
         return studentService.getAllStudent();
     }
--- student-system/springboot-student-system/springboot-student-system/src/main/java/com/springboot/springbootstudentsystem/service/impl/StudentServiceImpl.java
+++ student-system/springboot-student-system/springboot-student-system/src/main/java/com/springboot/springbootstudentsystem/service/impl/StudentServiceImpl.java
@@ -10,6 +10,11 @@ import java.util.List;
 @Service
 public class StudentServiceImpl implements StudentService {
     private StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     @Override
     public List<Student> getAllStudent() {
         return studentRepository.findAll();
