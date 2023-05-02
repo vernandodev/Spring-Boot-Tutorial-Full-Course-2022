3b1a77279ba4b9ccd54819815131b6e6fb8905f6
Richo Vernando <66782281+vernandodev@users.noreply.github.com>
2023-05-02 20:30:34 +0700

    update

--- springboot-angular/angular-frontend/src/app/app.module.ts
+++ springboot-angular/angular-frontend/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { EmployeeListComponent } from "./employee-list/employee-list.component";
 import { AppRoutingModule } from "./app-routing.module";
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 
 @NgModule({
-  declarations: [AppComponent, EmployeeListComponent],
+  declarations: [AppComponent, EmployeeListComponent, CreateEmployeeComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
