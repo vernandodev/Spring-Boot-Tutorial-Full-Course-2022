0b0ff5b59a7ef8915f19b584e46a116ad2387d2d
Richo Vernando <66782281+vernandodev@users.noreply.github.com>
2023-05-03 21:55:57 +0700

    update create employee component

--- springboot-angular/angular-frontend/src/app/create-employee/create-employee.component.ts
+++ springboot-angular/angular-frontend/src/app/create-employee/create-employee.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Employee } from "../employee";
+import { EmployeeService } from "../employee.service";
 
 @Component({
-  selector: 'app-create-employee',
-  templateUrl: './create-employee.component.html',
-  styleUrls: ['./create-employee.component.css']
+  selector: "app-create-employee",
+  templateUrl: "./create-employee.component.html",
+  styleUrls: ["./create-employee.component.css"],
 })
-export class CreateEmployeeComponent {
+export class CreateEmployeeComponent implements OnInit {
+  employees: Employee[];
 
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+
+  // define the method here
+  private getEmployees() {
+    this.employeeService.getEmployeeList().subscribe((data) => {
+      this.employees = data;
+    });
+  }
 }
