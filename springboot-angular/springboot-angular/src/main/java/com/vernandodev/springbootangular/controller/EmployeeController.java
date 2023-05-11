c178ce7a4aa7c73a3d0a931a1cbd9327ac2eda5f
Richo Vernando <66782281+vernandodev@users.noreply.github.com>
2023-05-11 21:30:26 +0700

    update

--- springboot-angular/springboot-angular/src/main/java/com/vernandodev/springbootangular/controller/EmployeeController.java
+++ springboot-angular/springboot-angular/src/main/java/com/vernandodev/springbootangular/controller/EmployeeController.java
@@ -2,13 +2,14 @@ package com.vernandodev.springbootangular.controller;
 
 import com.vernandodev.springbootangular.entity.Employee;
 import com.vernandodev.springbootangular.repository.EmployeeRepository;
+import com.vernandodev.springbootangular.response.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class EmployeeController {
@@ -18,6 +19,19 @@ public class EmployeeController {
     // get all employee
     @GetMapping("/employees")
     public List<Employee> getAllEmployee(){
-        return employeeRepository.findAll();
+        return employeeRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
+    }
+
+    // create employee rest api
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    // get employee by id
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
+        return ResponseEntity.ok(employee);
     }
 }
--- springboot-angular/springboot-angular/src/main/java/com/vernandodev/springbootangular/entity/Employee.java
+++ springboot-angular/springboot-angular/src/main/java/com/vernandodev/springbootangular/entity/Employee.java
@@ -2,13 +2,10 @@ package com.vernandodev.springbootangular.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
-import org.hibernate.annotations.GenericGenerator;
-
-import java.io.Serializable;
 
 @Entity
 @Table(name = "tbl-employee")
-public class Employee implements Serializable {
+public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -20,4 +17,48 @@ public class Employee implements Serializable {
     private String lastName;
     @Column(name = "email", nullable = false, unique = true, length = 50)
     private String emailId;
+
+    // contructor default
+    public Employee() {
+
+    }
+
+    // constructor
+    public Employee(String firstName, String lastName, String emailId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
 }
